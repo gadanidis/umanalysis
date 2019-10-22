50f04a53242d9589616f451a2b5f4795fe30b630
Tim Gadanidis <tim@gadanidis.ca>
2019-10-22 12:02:00 -0400

    initial commit

---
+++ analysis.R
@@ -0,0 +1,102 @@
+source("setup.R")
+
+# plotting
+theme_set(theme_bw(base_size = 18, base_family = "Libertinus Serif") %+replace%
+    theme( strip.background = element_rect(fill = "transparent")
+         , panel.background = element_rect(fill = "transparent")
+         , plot.background = element_rect(fill = "transparent", color = NA)
+         , panel.grid.major = element_blank()
+         , panel.grid.minor = element_blank()
+         , legend.background = element_rect(fill = "transparent")
+         , legend.box.background = element_rect(fill = "transparent")
+         )
+)
+
+library(RColorBrewer)
+myColors <- brewer.pal(3,"Set2")
+names(myColors) <- levels(dat$condition)
+colScale <- scale_colour_manual(name = "condition", values = myColors)
+fillScale <- scale_fill_manual(name = "condition", values = myColors)
+
+scales <- colnames(dat %>% select(feminine:friendly)) %>% set_names
+
+my_hist <- function(scale) {
+    ggplot(dat, aes(x = !!sym(scale) %>% as.numeric, fill = variant)) +
+        geom_bar(stat = "count", width = 0.8) +
+        facet_grid(~variant) +
+        geom_vline( data = aggregate( eval(parse(text = scale)) ~ variant
+                                    , data = dat
+                                    , mean
+                                    ) %>% set_names("variant", "the_mean")
+                  , mapping = aes(xintercept = the_mean)
+                  , colour = "#555555"
+                  , linetype = "dashed"
+                  , size = 0.5
+                  ) +
+        labs( y = "count"
+            , x = scale
+            ) +
+        guides(fill = F) +
+        scale_y_continuous(expand = expand_scale(mult = c(0, .05))) +
+        fillScale
+}
+hists <- scales %>% map(my_hist)
+
+my_diff <- function(scale) {
+    dat %>% group_by(variant, !!sym(scale)) %>%
+        summarize(n = n()) %>%
+        spread(variant, n) %>%
+        mutate(uh = uh - neither, um = um - neither) %>%
+        select(uh, um, !!sym(scale)) %>%
+        gather(uh, um, key = "variant", value = "cases") %>%
+        ggplot(aes(x = !!sym(scale) %>% as.numeric, y = cases, fill = variant)) +
+        geom_col(width = 0.8) +
+        geom_hline(yintercept = 0) +
+        facet_grid(~variant) +
+        labs( y = "difference"
+            , x = scale
+            ) +
+        guides(fill = F) +
+        ylim(c(-10,10)) +
+        fillScale
+}
+diffs <- scales %>% map(my_diff)
+
+# ordinal regression models with random effects
+
+helmert <- matrix(c(2/3, -1/3, -1/3, 0, -0.5, 0.5), ncol = 2)
+dummy <- matrix(c(-0.5, 0.5, 0, -0.5, 0, 0.5), ncol = 2)
+colnames(helmert) <- c("xVhm", "hVm")
+colnames(dummy) <- c("xVh", "xVm")
+
+contr.simp <- function(predictor){
+    contrasts(predictor) - (1 / length(levels(predictor)))
+}
+
+# make new dataset for regression
+
+ordinal_data <- dat %>%
+    mutate_if(is.character, as.factor) %>%
+    mutate_if(is.numeric, as.factor)
+
+# my_clmm
+
+my_clmm <- function(depvar) {
+    clmm(
+        formula = depvar ~ variant * voice +
+            (1|subject_id) + (1|stim),
+        contrasts = list(variant = contr.simp(ordinal_data$variant)),
+        data = ordinal_data
+    )
+}
+
+temp_clmm <- clmm(
+    formula = feminine ~ voice * variant + (1|subject_id) + (1|stim),
+    contrasts = list(variant = contr.simp(ordinal_data$variant)),
+    data = ordinal_data
+    )
+
+# map clmm
+scales <- colnames(ordinal_data %>% select(feminine:friendly)) %>% set_names
+clmms <- scales %>% map(~my_clmm(ordinal_data[[.]]))
+summaries <- simple_clmms %>% map(~summary(.))
---
+++ setup.R
@@ -0,0 +1,85 @@
+library(plyr)
+library(tidyverse)
+library(RJSONIO)
+library(jsonlite)
+library(ordinal)
+
+data_path <- "../results"   # path to the data
+files <- dir(data_path, pattern = "*.csv")
+scales <- c( "feminine"
+           , "masculine"
+           , "young"
+           , "queer"
+           , "canadian"
+           , "intelligent"
+           , "hesitant"
+           , "polite"
+           , "casual"
+           , "friendly"
+           )
+
+get_variant <- function (condition, stimulus) {
+    ifelse(condition == 1,
+        ifelse(stimulus == 1 | stimulus == 4, "uh",
+            ifelse(stimulus == 2 | stimulus == 5, "um", "neither")
+            ),
+        ifelse(condition == 2,
+            ifelse(stimulus == 1 | stimulus == 4, "neither",
+                ifelse(stimulus == 2 | stimulus == 5, "uh", "um")
+                ),
+            ifelse(stimulus == 1 | stimulus == 4, "um",
+                ifelse(stimulus == 2 | stimulus == 5, "neither", "uh")
+            )
+        )
+    )
+}
+
+raw <- files %>%
+    map(~ read_csv(file.path(data_path, .))) %>%
+    reduce(rbind.fill) %>%
+    mutate(voice = replace_na(voice, "r")) %>%
+    as_tibble()
+
+afterbrief <- raw %>%
+    select( subject_id
+          , gender
+          , age
+          , ethnicity
+          , responses
+          , voice
+          , condition
+          , type
+          , stim
+          ) %>%
+    filter(type == "afterbrief") %>%
+    mutate(responses = map(responses, ~ fromJSON(.) %>% as_tibble())) %>%
+    unnest(responses)
+
+dat <- raw %>%
+    select( subject_id
+          , gender
+          , age
+          , ethnicity
+          , responses
+          , voice
+          , condition
+          , type
+          , stim
+          ) %>%
+    filter(type == "quant-results", stim < 7) %>%
+    mutate(responses = map(responses, ~ fromJSON(.) %>% as_tibble())) %>%
+    unnest(responses) %>%
+    mutate(variant = get_variant(condition, stim)) %>%
+    rename(
+          feminine    = Q0
+        , masculine   = Q1
+        , young       = Q2
+        , queer       = Q3
+        , canadian    = Q4
+        , intelligent = Q5
+        , hesitant    = Q6
+        , polite      = Q7
+        , casual      = Q8
+        , friendly    = Q9
+    )
+
